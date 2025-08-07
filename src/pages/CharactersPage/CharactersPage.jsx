11c0dca87459809d675512f9208abead2e9e8b11
Anastasiya Andrushkevich <nastyayarosh147@gmail.com>
2025-08-07 17:16:10 +0300

    The Rick and Morty. Create character page.

--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -2,30 +2,91 @@ import { useEffect } from "react";
 import s from "./CharactersPage.module.css";
 import axios from "axios";
 import { useState } from "react";
+import { Link } from "react-router";
 
 export const CharactersPage = () => {
   const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchData = (url) => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCharacters(res.data.results);
+        setInfo(res.data.info);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.response.data.error);
+      });
+  };
 
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character").then((res) => {
-      setCharacters(res.data.results);
-    });
-  }, []); //side effect
+    fetchData("https://rickandmortyapi.com/api/character");
+  }, []);
+
+  const previousPageHandler = () => {
+    fetchData(info.prev);
+  };
+  const nextPageHandler = () => {
+    fetchData(info.next);
+  };
+  const searchHandler = (event) => {
+    const value = event.currentTarget.value;
+    fetchData(`https://rickandmortyapi.com/api/character?name=${value}`);
+  };
 
   return (
     <div className={"pageContainer"}>
-      <h1 className={"pageTitle"}>CharactersPage</h1>
-      {characters.length && (
-        <div className={s.characters}>
-          {characters.map((character) => {
-            return (
-              <div key={character.id} className={s.character}>
-                <div className={s.characterLink}>{character.name}</div>
-                <img src={character.image} alt={`${character.name} avatar`} />
-              </div>
-            );
-          })}
-        </div>
+      <h1 className={"pageTitle"}>CharacterPage</h1>
+      <input
+        type="search"
+        className={s.search}
+        onChange={searchHandler}
+        placeholder="Search..."
+      />
+      {error && <div className="errorMessage">{error}</div>}
+
+      {!error && characters.length && (
+        <>
+          {
+            <div className={s.characters}>
+              {characters.map((character) => {
+                return (
+                  <div key={character.id} className={s.character}>
+                    <Link
+                      to={`/characters/${character.id}`}
+                      className={s.characterLink}
+                    >
+                      {character.name}
+                    </Link>
+                    <img
+                      src={character.image}
+                      alt={`${character.name} avatar`}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          }
+          <div className={s.buttonContainer}>
+            <button
+              className="linkButton"
+              disabled={info.prev === null}
+              onClick={previousPageHandler}
+            >
+              Назад
+            </button>
+            <button
+              className="linkButton"
+              disabled={info.next === null}
+              onClick={nextPageHandler}
+            >
+              Вперед
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
